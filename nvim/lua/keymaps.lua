91b81120e107a7ad4f1a02f9f5d305187d747fb7
Yadhukrishnan P <yadhukrishnannair99@gmail.com>
2024-09-12 23:24:32 -0700

    pilot commit

---
+++ nvim/lua/keymaps.lua
@@ -0,0 +1,4 @@
+vim.g.mapleader = " "
+vim.keymap.set('n', '<Esc>', vim.cmd.Ex) 
+
+
---
+++ nvim/lua/lazy.lua
@@ -0,0 +1,29 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+local plugins = {
+
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	
+	{
+		'nvim-telescope/telescope.nvim', tag = '0.1.8',
+		dependencies = { 'nvim-lua/plenary.nvim' }
+	},
+	
+	{ "ThePrimeagen/harpoon" }
+}
+
+local opts = {}
+
+require("lazy").setup(plugins)
